a7540a2bec1e9296c7fe2133a96ceadeab294ce5
Aayush <bhaskarsharma64047@gmail.com>
2023-04-08 12:02:06 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -22,7 +22,7 @@ async def maintainers(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Developer💻", url=f"https://t.me/sigma_male_007")
+                                InlineKeyboardButton("BOT LIST", url=f"https://t.me/mdisk_bots")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("FOLLOW ME", url=f"https://GITHUB.COM/adarsh-goel")
+                                InlineKeyboardButton("24×7 MOVIES ", url=f"https://t.me/blackest_harbour")
                             ]
                         ]
                     ),
