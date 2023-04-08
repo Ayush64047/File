f5f99ecde608e79d9421ef9edc669a8a06735575
Aayush <bhaskarsharma64047@gmail.com>
2023-04-08 11:21:02 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,14 +9,14 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('17954446'))
-    API_HASH = str(getenv('6283885086:AAFGotuIzOaLrsERJWo6M1xBAc6jCxmtLPY'))
-    BOT_TOKEN = str(getenv('5959633763:AAF-QmxkcwoCpV8cCNDEUAfYWGit_Hl7NbE'))
+    API_ID = int(getenv('API_ID','17954446'))
+    API_HASH = str(getenv('API_HASH','6283885086:AAFGotuIzOaLrsERJWo6M1xBAc6jCxmtLPY'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN''5959633763:AAF-QmxkcwoCpV8cCNDEUAfYWGit_Hl7NbE'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '30'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('-1001738180086'))
-    PORT = int(getenv('PORT', 8080))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL''-1001738180086'))
+    PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', ''))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("", "1413071149").split())  
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('mongodb+srv://Ayush64047:Ayush64047@cluster5.2xihv2h.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('z_harbour', None))
+    DATABASE_URL = str(getenv('DATABASE_URL','mongodb+srv://Ayush64047:Ayush64047@cluster5.2xihv2h.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'z_harbour'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
